ad76dbb7b3e592798283b7d17ba4679d1007ac00
Mitchel <febemitch1@gmail.com>
2023-02-25 13:27:06 +0100

    removed unnecessary packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import middleware from "./middleware/index.js";
-import path from 'path';
 import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
 app.use(cors({ exposedHeaders: ["links"] }))
 
 app.use(express.static('src/static'));
